c4f7127c484684a4811b79d5bb74443aa6d08230
Jayantyadav26 <95078136+Jayantyadav26@users.noreply.github.com>
2024-08-30 01:47:30 +0530

    backend v1 final

--- backend1/src/Game.ts
+++ backend1/src/Game.ts
@@ -1,6 +1,6 @@
 import WebSocket from "ws";
 import { BLACK, Chess } from 'chess.js'
-import { GAME_OVER, MOVE } from "./message";
+import { GAME_OVER, INIT_GAME, MOVE } from "./message";
 
 export class Game{
     public player1:WebSocket;
@@ -14,6 +14,18 @@ export class Game{
         this.player2 = player2;
         this.board = new Chess();
         this.startTime = new Date();
+        this.player1.send(JSON.stringify({
+            type: INIT_GAME,
+            payload:{
+                color:"white"
+            }
+        }))
+        this.player2.send(JSON.stringify({
+            type: INIT_GAME,
+            payload:{
+                color:"black"
+            }
+        }))
     }
 
     makeMove(socket : WebSocket , move: {
